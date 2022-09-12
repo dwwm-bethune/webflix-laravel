f51a17110e3f0d8e6dd917ed68d7af26d30e1581
Matthieu Mota <matthieumota@gmail.com>
2022-09-12 16:59:29 +0200

    Vite

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <h1>Coucou {{ $name }}</h1>
